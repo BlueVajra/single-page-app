868306ebdf2444b9309ac701598c0b40312caf5f
Cory Leistikow <cory.leistikow@gmail.com>
2014-06-11 16:41:54 -0600

    [#73067276] User can add a new person
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- app/assets/javascripts/people_app.js
+++ app/assets/javascripts/people_app.js
@@ -1,16 +1,37 @@
 window.PeopleApp = {
   initialize: function () {
 
-    $.getJSON("/api/people", function (data) {
-      var people = []
-      $(data._embedded.people).each(function (i) {
-        people[i] = this
-      });
-      html = JST['templates/people']({people: people});
-      $("main .container div[data-container='people']").append(html)
-
+    $.getJSON("/api/people", function(data){
+      PeopleApp.renderPeople(data)
     });
 
+    $('.create-form').submit( function(event){
+      PeopleApp.createPerson(event)
+    });
+  },
+
+  renderPeople: function (data) {
+    html = JST['templates/people']({people: data._embedded.people});
+    $("main .container div[data-container='people']").append(html)
+  },
+
+  renderPerson: function(response){
+    html = JST['templates/person']({person: response});
+    $("main .container div[data-container='people']").append(html);
+  },
+
+  createPerson: function(event){
+    var person = {
+      "first_name": $('input.first_name').val(),
+      "last_name": $('input.last_name').val(),
+      "address": $('textarea.address').val()
+    }
+
+    $.post("/api/people", JSON.stringify(person), function(response){
+      PeopleApp.renderPerson(response);
+    }, "json");
+
+    event.preventDefault();
   }
 }
 
--- spec/features/contact_manager_spec.rb
+++ spec/features/contact_manager_spec.rb
@@ -12,4 +12,16 @@ feature 'The one-page contact manager app' do
     expect(page).to have_content("Joe Example")
     expect(page).to have_content("Edna Example")
   end
+
+  scenario 'User adds a person', js: true do
+    visit '/'
+    fill_in 'First name', with: 'Joe'
+    fill_in 'Last name', with: 'Schmoe'
+    fill_in 'Address', with: '22 Pearl St'
+    click_button 'Add Person'
+
+    expect(page).to have_content 'Joe'
+    expect(page).to have_content 'Schmoe'
+    expect(page).to have_content '22 Pearl St'
+  end
 end
